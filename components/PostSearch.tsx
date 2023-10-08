700751008509402938c9cf229e47cedfd2f76d9f
Pavel <nightcharm@mail.ru>
2023-10-08 06:41:38 +0300

    add swr, remake Posts in swr

--- components/PostSearch.tsx
+++ components/PostSearch.tsx
@@ -25,18 +25,47 @@
 // }
  
 // export default PostSearch;
-//-------zustand
-"use client"
 
-import { usePosts } from "@/store";
+//-------zustand
+// "use client"
+
+// import { usePosts } from "@/store";
+// import { FormEventHandler, useState } from "react";
+
+// const PostSearch = () => {
+//     const [search, setSearch]=useState("");
+//     const getPostsBySearch = usePosts(state => state.getPostsBySearch)
+//     const handleSubmit: FormEventHandler<HTMLFormElement>= async (event) => {
+//      event?.preventDefault();
+//     await getPostsBySearch(search);
+//     }
+
+//     return ( <form onSubmit={handleSubmit}>
+// <input
+//  type="seacrh"
+//    placeholder="search"
+//     value={search}
+//       onChange={event => setSearch(event.target.value)}/>
+// <button type="submit">Поиск</button>
+//     </form> );
+// }
+ 
+// export default PostSearch;
+
+//------c swr библиоткой
+
+"use client"
+import { getPostsBySearch } from "@/servises/getPosts";
 import { FormEventHandler, useState } from "react";
+import useSWR from "swr";
 
 const PostSearch = () => {
+  const {mutate}=useSWR("posts");
     const [search, setSearch]=useState("");
-    const getPostsBySearch = usePosts(state => state.getPostsBySearch)
     const handleSubmit: FormEventHandler<HTMLFormElement>= async (event) => {
      event?.preventDefault();
-    await getPostsBySearch(search);
+    const posts=await getPostsBySearch(search);
+    mutate(posts);
     }
 
     return ( <form onSubmit={handleSubmit}>
--- components/Posts.tsx
+++ components/Posts.tsx
@@ -15,23 +15,46 @@
 // }
  
 // export default Posts;
+
 //--zustand
+// "use client"
+// import { usePosts } from "@/store";
+// import Link from "next/link";
+// import { useEffect } from "react";
+// import { shallow } from "zustand/shallow";
+
+// const Posts = () => {
+
+    
+//     const [posts, loading, getPosts]=usePosts((state) => [state.posts, state.loading, state.getPosts], shallow);
+
+//     useEffect(() => {
+//         getPosts()
+//     },[getPosts]);
+    
+//     return loading ? (  
+//     <h3>Loading</h3>) :
+//         (<ul>
+//         {posts.map((post: any) => {
+//         return    <li key={post.id}><Link href= {`/blog/${post.id}`}>{post.title}</Link></li>
+//         })}
+//     </ul>
+//      );
+// }
+ 
+// export default Posts;
+
+//---c библиотекой swr 
+
 "use client"
-import { usePosts } from "@/store";
+import getPosts from "@/servises/getPosts";
 import Link from "next/link";
-import { useEffect } from "react";
-import { shallow } from "zustand/shallow";
+import useSWR from "swr";
 
 const Posts = () => {
 
-    
-    const [posts, loading, getPosts]=usePosts((state) => [state.posts, state.loading, state.getPosts], shallow);
-
-    useEffect(() => {
-        getPosts()
-    },[getPosts]);
-    
-    return loading ? (  
+const {data:posts, isLoading}=useSWR("posts", getPosts);
+    return isLoading ? (  
     <h3>Loading</h3>) :
         (<ul>
         {posts.map((post: any) => {
